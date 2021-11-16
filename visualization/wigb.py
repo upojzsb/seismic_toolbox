67d39accc6c9cb581b5d012734584505f1ff7d56
UPO-JZSB <zgzhaoliang@gmail.com>
2021-11-16 21:43:08 +0800

    Fixed a problem which may cause inconsistent display  Fixed a problem which may change original data

--- visualization/wigb.py
+++ visualization/wigb.py
@@ -1,16 +1,21 @@
-# Created by UPO-JZSB on Jan/17/2020
+# Created by UPO-JZSB on Jan/17/2021
 # Released Under GPL-v3.0 License
 
-# Version 0.1 is released on Jan/17/2020
+# Version 0.1 is released on Jan/17/2021
 # Implementation of wigb
 
-# Version 0.2 is released on Jan/19/2020
+# Version 0.2 is released on Jan/19/2021
 # Add parameter direction - decide the plot direction
 
 # Version 0.3 is released on Jan/20/2021
 # Replace `plot' by `fill_between' function to boost the filling procession
 
+# Version 0.4 is released on Nov/16/2021
+# Fixed a problem which may cause inconsistent display
+# Fixed a problem which may change original data
+
 import numpy as np
+import copy
 import matplotlib.pyplot as plt
 
 
@@ -31,6 +36,7 @@ def wigb(a=None, scale=1, x=None, z=None, a_max=None, figsize=(30, 15), no_plot=
     :return: if no_plot is False, plot the seismic data, otherwise, do not plot immediately,
             users can adjust plot parameters outside
     """
+    a = copy.copy(a)
     n_data, n_trace = a.shape
 
     if x is None:
@@ -38,7 +44,7 @@ def wigb(a=None, scale=1, x=None, z=None, a_max=None, figsize=(30, 15), no_plot=
     if z is None:
         z = np.arange(n_data)
     if a_max is None:
-        a_max = np.mean(np.max(a, axis=0))
+        a_max = np.max(np.max(a, axis=0))
     if direction not in ['Horizontal', 'Vertical']:
         raise ValueError('Direction must be \'Horizontal\' or \'Vertical\'')
 
@@ -58,15 +64,17 @@ def wigb(a=None, scale=1, x=None, z=None, a_max=None, figsize=(30, 15), no_plot=
         plt.gca().invert_yaxis()
 
         for index_x in range(n_trace):
+            zero_offset = index_x*dx
             trace = a[:, index_x]
-            plt.plot(index_x * dx + trace, z, 'k-', linewidth=2)
-
+            plt.plot(zero_offset + trace, z, 'k-', linewidth=2)
             plt.fill_betweenx(
                 np.array([y * dz for y in range(n_data)]),
-                np.zeros_like(np.arange(n_data)) + index_x * dx,
-                trace + index_x * dx,
+                np.zeros_like(np.arange(n_data)) + zero_offset,
+                trace + zero_offset,
                 where=trace > 0,
-                color='k'
+                interpolate=True,
+                color='k',
+                antialiased=True
             )
 
     elif direction == 'Horizontal':
@@ -75,15 +83,18 @@ def wigb(a=None, scale=1, x=None, z=None, a_max=None, figsize=(30, 15), no_plot=
         plt.gca().invert_yaxis()
 
         for index_z in range(n_trace):
+            zero_offset = index_z*dx
             trace = a[:, index_z]
-            plt.plot(z, index_z * dx + trace, 'k-', linewidth=2)
+            plt.plot(z, zero_offset + trace, 'k-', linewidth=2)
 
             plt.fill_between(
                 np.array([y * dz for y in range(n_data)]),
-                np.zeros_like(np.arange(n_data)) + index_z * dx,
-                trace + index_z * dx,
+                np.zeros_like(np.arange(n_data)) + zero_offset,
+                trace + zero_offset,
                 where=trace > 0,
-                color='k'
+                interpolate=True,
+                color='k',
+                antialiased=True
             )
 
     if not no_plot:
